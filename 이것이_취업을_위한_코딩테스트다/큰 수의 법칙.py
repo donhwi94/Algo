6e26d9b349d9d6de3c437900c0dc0dbe957ca0d3
donhwi94 <ehsgnl123@naver.com>
2021-06-16 23:23:54 +0900

    실전1) 큰 수의 법칙 풀이(m이 큰 경우)

--- 이것이_취업을_위한_코딩테스트다/큰 수의 법칙.py
+++ 이것이_취업을_위한_코딩테스트다/큰 수의 법칙.py
@@ -8,15 +8,11 @@ second = data[1]
 
 result = 0
     
-while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -= 1
-    if m == 0:
-        break
-    result += second
-    m -= 1
+# 가장 큰 수가 더해지는 횟수 구하기
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result += count * first
+result += (m - count) * second
 
 print(result)
